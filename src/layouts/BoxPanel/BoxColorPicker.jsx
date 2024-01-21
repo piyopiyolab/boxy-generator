2cb160feef7fa28399753092863df8ad1bab025e
Marion <piouceau.marion@gmail.com>
2024-01-21 15:43:05 +0100

    input color ok

--- src/layouts/BoxPanel/BoxColorPicker.jsx
+++ src/layouts/BoxPanel/BoxColorPicker.jsx
@@ -1,8 +1,39 @@
+import { useDispatch } from "react-redux"
+import { updateBoxValue } from "../../redux/reducers/boxProperties"
 
 
-function BoxColorPicker() {
+
+
+function BoxColorPicker({ inputData }) {
+
+    const dispatch = useDispatch()
+    function handleInputs(e) {
+        dispatch(updateBoxValue({
+            inputNumber: inputData.inputNumber,
+            value: e.target.value,
+        }))
+    }
+
+
+
     return (
-        <div>BoxColorPicker</div>
+        <div className="mt-3">
+            <p>{inputData.name}</p>
+            <div className="flex mt-2">
+                <input
+                    value={inputData.value}
+                    onChange={handleInputs}
+                    className="flex-grow border py-1 px-2 focus:outline-1 outline-gray-400"
+                    type="text"
+                />
+                <input
+                    value={inputData.value}
+                    onChange={handleInputs}
+                    type="color"
+                    className="cursor-pointer h-[40px]" />
+            </div>
+
+        </div>
     )
 }
 export default BoxColorPicker
